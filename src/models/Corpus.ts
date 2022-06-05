3b588a02b83339efcdfbb53b8af2c541c0991e8b
Thibault Kruse <thibault.kruse@gmx.de>
2022-06-05 21:09:11 +0900

    Reduce variants

--- src/models/Corpus.ts
+++ src/models/Corpus.ts
@@ -6,23 +6,23 @@ import { sentence } from "satzbau";
 import { Person, AllPersons } from "./Person";
 
 const nounsGenerator = [
-  () => new Noun("das Haus,die Häuser,des Hauses", "house"),
+  // () => new Noun("das Haus,die Häuser,des Hauses", "house"),
   () => new Noun("das Bett,-en,-es", "bed"),
-  () => new Noun("das Bild,-er,-es", "picture"),
-  () => new Noun("das Messer,-,-s", "knife"),
-  () => new Noun("das Hemd,-en,-es", "shirt"),
-  () => new Noun("das Bier,-e,-e", "beer"),
+  // () => new Noun("das Bild,-er,-es", "picture"),
+  // () => new Noun("das Messer,-,-s", "knife"),
+  // () => new Noun("das Hemd,-en,-es", "shirt"),
+  // () => new Noun("das Bier,-e,-e", "beer"),
   () => new Noun("der Tisch,-e,-es", "table"),
-  () => new Noun("der Teller,-,-s", "plate"),
-  () => new Noun("der Koffer,-,-s", "suitcase"),
-  () => new Noun("der Kuchen,-,-s", "cake"),
-  () => new Noun("der Schuh,-e,-es", "shoe"),
-  () => new Noun("der Wein,-e,-es", "wine"),
+  // () => new Noun("der Teller,-,-s", "plate"),
+  // () => new Noun("der Koffer,-,-s", "suitcase"),
+  // () => new Noun("der Kuchen,-,-s", "cake"),
+  // () => new Noun("der Schuh,-e,-es", "shoe"),
+  // () => new Noun("der Wein,-e,-es", "wine"),
   () => new Noun("die Lampe,-n,-n", "lamp"),
-  () => new Noun("die Tasse,-n,-", "cup"),
-  () => new Noun("die Gabel,-n,-", "fork"),
-  () => new Noun("die Socke,-n,-", "sock"),
-  () => new Noun("die Soße,-n,-", "sauce"),
+  // () => new Noun("die Tasse,-n,-", "cup"),
+  // () => new Noun("die Gabel,-n,-", "fork"),
+  // () => new Noun("die Socke,-n,-", "sock"),
+  // () => new Noun("die Soße,-n,-", "sauce"),
 ];
 
 // const pronounsGenerator = [
@@ -43,20 +43,20 @@ const sentenceGenerators = [
       `I find ${noun.renderEN()}.`
     );
   },
-  function (noun: Noun) {
-    return new Sentence(
-      noun,
-      sentence`Du hast ${noun.accusative().renderDE()}`,
-      `You have ${noun.renderEN()}.`
-    );
-  },
-  function (noun: Noun) {
-    return new Sentence(
-      noun,
-      sentence`Er kauft ${noun.accusative().renderDE()}`,
-      `He buys ${noun.renderEN()}.`
-    );
-  },
+  // function (noun: Noun) {
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Du hast ${noun.accusative().renderDE()}`,
+  //     `You have ${noun.renderEN()}.`
+  //   );
+  // },
+  // function (noun: Noun) {
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Er kauft ${noun.accusative().renderDE()}`,
+  //     `He buys ${noun.renderEN()}.`
+  //   );
+  // },
   function (noun: Noun) {
     return new Sentence(
       noun,
@@ -64,20 +64,20 @@ const sentenceGenerators = [
       `I help ${noun.renderEN()}.`
     );
   },
-  function (noun: Noun) {
-    return new Sentence(
-      noun,
-      sentence`Ich schlafe mit ${noun.dative().renderDE()}`,
-      `I sleep with ${noun.renderEN()}.`
-    );
-  },
-  function (noun: Noun) {
-    return new Sentence(
-      noun,
-      sentence`Er steht vor ${noun.dative().renderDE()}`,
-      `He stands in front of ${noun.renderEN()}.`
-    );
-  },
+  // function (noun: Noun) {
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Ich danke ${noun.dative().renderDE()}`,
+  //     `I thank ${noun.renderEN()}.`
+  //   );
+  // },
+  // function (noun: Noun) {
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Er steht vor ${noun.dative().renderDE()}`,
+  //     `He stands in front of ${noun.renderEN()}.`
+  //   );
+  // },
   function (noun: Noun) {
     const articleEN = noun.isPlural() ? "are" : "is";
     const articleDE = noun.isPlural() ? "sind" : "ist";
@@ -88,36 +88,36 @@ const sentenceGenerators = [
       `${pointerEN} ${articleEN} ${noun.renderEN()}.`
     );
   },
-  function (noun: Noun) {
-    const articleEN = noun.isPlural() ? "were" : "was";
-    const articleDE = noun.isPlural() ? "waren" : "war";
-    return new Sentence(
-      noun,
-      sentence`Mein Geschenk ${articleDE} ${noun.nominative().renderDE()}`,
-      `My gift ${articleEN} ${noun.renderEN()}.`
-    );
-  },
-  function (noun: Noun) {
-    const articleEN = noun.isPlural() ? "are" : "is";
-    const articleDE = noun.isPlural() ? "sind" : "ist";
-    return new Sentence(
-      noun,
-      sentence`Wo ${articleDE} ${noun.nominative().renderDE()}`.ask(),
-      `Where ${articleEN} ${noun.renderEN()}?`
-    );
-  },
+  // function (noun: Noun) {
+  //   const articleEN = noun.isPlural() ? "were" : "was";
+  //   const articleDE = noun.isPlural() ? "waren" : "war";
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Mein Geschenk ${articleDE} ${noun.nominative().renderDE()}`,
+  //     `My gift ${articleEN} ${noun.renderEN()}.`
+  //   );
+  // },
+  // function (noun: Noun) {
+  //   const articleEN = noun.isPlural() ? "are" : "is";
+  //   const articleDE = noun.isPlural() ? "sind" : "ist";
+  //   return new Sentence(
+  //     noun,
+  //     sentence`Wo ${articleDE} ${noun.nominative().renderDE()}`.ask(),
+  //     `Where ${articleEN} ${noun.renderEN()}?`
+  //   );
+  // },
 ];
 
 const attributes = [
   new Attribute("klein", "small"),
   new Attribute("neu", "new"),
-  new Attribute("schön", "nice"),
-  new Attribute("schwarz", "black"),
-  new Attribute("rot", "red"),
-  new Attribute("hell", "bright"),
-  new Attribute("billig", "cheap"),
-  new Attribute("leicht", "light"),
-  new Attribute("gut", "good"),
+  // new Attribute("schön", "nice"),
+  // new Attribute("schwarz", "black"),
+  // new Attribute("rot", "red"),
+  // new Attribute("hell", "bright"),
+  // new Attribute("billig", "cheap"),
+  // new Attribute("leicht", "light"),
+  // new Attribute("gut", "good"),
 ];
 
 export class Corpus {
@@ -126,7 +126,7 @@ export class Corpus {
       nounsGenerator[Math.floor(Math.random() * nounsGenerator.length)]();
 
     const newAttributes: Attribute[] = [];
-    for (let i = 0; i < Math.floor(Math.random() * 3); i++) {
+    for (let i = 0; i < Math.floor(Math.random() * 2); i++) {
       const randomAttribute =
         attributes[Math.floor(Math.random() * attributes.length)];
       if (!newAttributes.includes(randomAttribute)) {
